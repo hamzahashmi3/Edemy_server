c9981f914375e304182af0cafd6d9f08305ea011
hamzahashmi3 <technocannons3@gmail.com>
2022-12-15 21:08:09 +0500

    first commit

---
+++ middlewares/index.js
@@ -0,0 +1,23 @@
+import { expressjwt } from "express-jwt";
+import User from "../models/user.model";
+
+export const requireSignin = expressjwt({
+  getToken: (req, res) => req.cookies.token,
+  secret: process.env.JWT_SECRET,
+  algorithms: ["HS256"],
+});
+
+export const isInstructor = async (req, res, next) => {
+    try {
+        console.log(req.auth);
+      const user = await User.findById(req.auth._id).exec();
+      if (!user.role.includes("Instructor")) {
+        return res.sendStatus(403);
+      } else {
+        next();
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  
