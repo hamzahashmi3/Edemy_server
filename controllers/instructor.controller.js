4a99ea1894b2201d7fd0e31c90aa88064ac3143e
hamzahashmi3 <technocannons3@gmail.com>
2022-12-21 12:34:27 +0500

    undefined4 feat(backend complete): backend complete

--- controllers/instructor.controller.js
+++ controllers/instructor.controller.js
@@ -97,3 +97,28 @@ export const instructorCourses = async (req, res) => {
     }
   };
   
+export const instructorBalance = async (req, res) => {
+  try {
+    let user = await User.findById(req.auth._id).exec();
+    const balance = await stripe.balance.retrieve({
+      stripeAccount: user.stripe_account_id,
+    });
+    res.json(balance);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+
+export const instructorPayoutSettings = async (req, res) => {
+  try {
+    const user = await User.findById(req.auth._id).exec();
+    const loginLink = await stripe.accounts.createLoginLink(
+      user.stripe_seller.id,
+      { redirect_url: process.env.STRIPE_SETTINGS_REDIRECT }
+    );
+    res.json(loginLink.url);
+  } catch (err) {
+    console.log("stripe payout settings login link err => , err");
+  }
+};
